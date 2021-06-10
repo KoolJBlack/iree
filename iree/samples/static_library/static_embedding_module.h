99ea139cb1ca7b95878bd79853d1119823f4b69e
Kojo Acquah <kooljblack@google.com>
2021-06-10 15:33:11 -0700

    removed device static, moving to single file run

--- iree/samples/static_library/static_embedding_module.h
+++ iree/samples/static_library/static_embedding_module.h
@@ -1,7 +1,16 @@
-// Auto-generated static library header file.
+// === [simple_mul_dispatch_0] static library ===
+//
+// To use:
+//  - Include this header and generated object into your program
+//  - At runtime: retrieve library name from host binary
+//  - Query library from simple_mul_dispatch_0_library_query()<<
+//  - Feed library into static_library_loader
+//
+// === Automatically generated file. DO NOT EDIT! ===
 
 #ifndef IREE_GENERATED_STATIC_EXECUTABLE_LIBRARY_SIMPLE_MUL_DISPATCH_0_
 #define IREE_GENERATED_STATIC_EXECUTABLE_LIBRARY_SIMPLE_MUL_DISPATCH_0_
+
 #include "iree/hal/local/executable_library.h"
 
 #if __cplusplus
